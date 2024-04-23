b05021cbb00c2fdc43bdaa20c28cbf51d7a070b7
Muhammad Irfan Nur Hakim <muhammadirfannh36@gmail.com>
2024-04-23 18:41:38 +0700

    JOBSHEET 9 -  AUTH

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,11 +4,11 @@
 
 <div class="card">
   <div class="card-header">
-    <h3 class="card-title">Halo, apakabar!!!</h3>
+    <h3 class="card-title">Halo, Muhammad Irfan Nur Hakim.</h3>
     <div class="card-tools"></div>
   </div>
   <div class="card-body">
-    Selamat datang semua, ini adalah halaman utama dari aplikasi ini.
+    Silahkan melakukan aktivitas anda hari ini pada website POS (Point Of Sales), ini merupakan halaman dashboard anda.
   </div>
 </div>
 @endsection
