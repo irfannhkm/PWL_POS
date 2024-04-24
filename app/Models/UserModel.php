32a5cbc089680d72678bd29a64da1a9acf2febf1
Muhammad Irfan Nur Hakim <muhammadirfannh36@gmail.com>
2024-04-24 09:24:34 +0700

    JOBSHEET 10 | PRAKTIKUM 1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,21 +2,29 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class UserModel extends \Illuminate\Foundation\Auth\User
+class UserModel extends Authenticatable implements JWTSubject
 {
     use HasFactory;
-
-    protected $primaryKey = 'user_id'; //mendefinisikan nama tabel yang digunakan oleh model ini
-    protected $table = 'm_user'; //mendefinisikan primary key dari tabel yang digunakan
-
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
 
     public function level(): BelongsTo
     {
         return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
     }
+    public function getJWTIdentifier(){
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims(){
+        return [];
+    }
+    protected $primaryKey = 'user_id'; //mendefinisikan nama tabel yang digunakan oleh model ini
+    protected $table = 'm_user'; //mendefinisikan primary key dari tabel yang digunakan
+
 }
